f1195f04c134e97abba2a2992d1cb1f5a2621bd6
Amine derbal <aminedoran31@gmail.com>
2023-06-26 11:55:03 +0100

    Create models for tables

---
+++ app/models/food.rb
@@ -0,0 +1,11 @@
+class Food < ApplicationRecord
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_many :recipe_foods, class_name: "RecipeFood", foreign_key: "food_id"
+  has_many :recipes, through: :recipe_foods
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :recipes, class_name: "Recipe", foreign_key: "user_id"
+  has_many :foods, class_name: "Food", foreign_key: "user_id"
+  validates :name, presence: true
+end

38810c220f86e8da0a942e85059ebfd7fb7fa04a
Amine derbal <aminedoran31@gmail.com>
2023-06-27 10:01:58 +0100

    Add attribute for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, class_name: "RecipeFood", foreign_key: "recipe_id"
   has_many :foods, through: :recipe_foods
 
+  accepts_nested_attributes_for :recipe_foods
+
   validates :name, presence: true
   validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }

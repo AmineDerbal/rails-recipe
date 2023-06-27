c53de088f70fd2fca3f0b06d4381fb616cd07fb0
Amine derbal <aminedoran31@gmail.com>
2023-06-27 12:08:19 +0100

    Add public_recipes controller

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,10 @@
+class PublicRecipesController < ApplicationController
+  layout 'standard'
+  before_action :authenticate_user!
+
+  def index
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
+
+  end
+
+end

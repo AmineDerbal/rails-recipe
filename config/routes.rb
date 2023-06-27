35a1c9d721b260c79b45c07fe0e279f8ef892cc6
Amine derbal <aminedoran31@gmail.com>
2023-06-27 13:29:38 +0100

    Add route for sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+   # Add the following route for signing out
+  resources :users, only: [:show]
+
   resources :recipes, except: [:update]
   resources :public_recipes, except: [:update]
 
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root 'public_recipes#index'
 
 end

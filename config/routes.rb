cceadd053ea60579dc779f2e8b6d7970d442d6fa
Amine derbal <aminedoran31@gmail.com>
2023-06-27 12:07:57 +0100

    Add public_recipes route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :recipes, except: [:update]
+  resources :public_recipes, except: [:update]
 
   # Defines the root path route ("/")
   # root "articles#index"

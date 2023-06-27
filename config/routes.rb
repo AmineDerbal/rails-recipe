2a1d820fccf17179348d5353c265dc8e37dd67cd
Amine derbal <aminedoran31@gmail.com>
2023-06-27 10:00:15 +0100

    Add routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :recipes, except: [:update]
+
   # Defines the root path route ("/")
   # root "articles#index"
+
+
 end

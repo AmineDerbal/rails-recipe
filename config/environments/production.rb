8651940c9a2cccefd7aaa1c90a7cf50a41d57e4b
Amine derbal <aminedoran31@gmail.com>
2023-08-10 17:55:40 +0100

    Config production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,23 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "recipe_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {  host: 'https://rails-recipe-apj7.onrender.com/' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'rails-recipe-apj7.onrender.com',
+    user_name:            ENV['EMAIL_NAME'],
+    password:             ENV['EMAIL_PASSWORD'], 
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
